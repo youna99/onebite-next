12632809c8893d8c64476401f1de8518161ec78b
youna <dbsk0108@gmail.com>
2024-12-11 21:15:07 +0900

    네비게이팅

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,13 +1,28 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const onClickButton = () => {
+    router.push('/test');
+  };
   return (
     <>
       {/* 루트 컴포넌트로 모든 페이지 컴포넌트의 부모컴포넌트이다. */}
-
       {/* 공통 컴포넌트를 넣을 수 있다 */}
-      <header>글로벌 헤더</header>
+
+      <header>
+        <Link href={'/'}>index</Link>
+        &nbsp;
+        <Link href={'/search'}>search</Link>
+        &nbsp;
+        <Link href={'/book/1'}>book/1</Link>
+        <div>
+          <button onClick={onClickButton}>/test 페이지로 이동</button>
+        </div>
+      </header>
       <Component {...pageProps} />
     </>
   );
