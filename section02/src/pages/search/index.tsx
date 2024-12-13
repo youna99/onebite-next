2c043080bc81820c422374242dc90c7ea9d6207f
youna <dbsk0108@gmail.com>
2024-12-13 17:02:42 +0900

    스타일 적용

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,16 +1,16 @@
 import SearchableLayout from '@/components/searchable-layout';
-import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
+import books from '@/mock/books.json';
+import BookItem from '@/components/book-item';
 
 export default function Page() {
-  // 라우터 객체 저장
-  const router = useRouter();
-  console.log(router);
-
-  //   const q = router.query.q;
-  const { q } = router.query;
-
-  return <h1>Search {q}</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
