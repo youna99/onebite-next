6bf069dfedcab235c6030e02c2f4a1a0385772c6
youna <dbsk0108@gmail.com>
2024-12-16 22:36:33 +0900

    SSR 기초 사용법

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -4,8 +4,23 @@ import style from './index.module.css';
 import SearchableLayout from '@/components/searchable-layout';
 import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
+import { InferGetServerSidePropsType } from 'next';
 
-export default function Home() {
+// 페이지 컴포넌트보다 먼저 실행되어, 컴포넌트에 필요한 데이터 불러오는 함수
+// 사전 렌더링하는 과정에서 딱 한번만 실행되므로 오직 서버측에서만 실행되는 함수
+export const getServerSideProps = () => {
+  const data = 'hello';
+
+  return {
+    props: { data },
+  };
+};
+export default function Home({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  // next에서 제공하는 타입으로 타입을 자동 추천해줌
+  console.log(data);
+
   return (
     <div className={style.container}>
       <section>
