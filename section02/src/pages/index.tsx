7fd35c4833f0b63aaa6b8356cdeff5321f581ffd
youna <dbsk0108@gmail.com>
2024-12-17 00:20:36 +0900

    SSR 방식으로 데이터패칭

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -2,36 +2,47 @@
 import { ReactNode } from 'react';
 import style from './index.module.css';
 import SearchableLayout from '@/components/searchable-layout';
-import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
 import { InferGetServerSidePropsType } from 'next';
+import fetchBooks from '@/lib/fetch-books';
+import fetchRandomBooks from '@/lib/fetch-random-books';
 
 // 페이지 컴포넌트보다 먼저 실행되어, 컴포넌트에 필요한 데이터 불러오는 함수
 // 사전 렌더링하는 과정에서 딱 한번만 실행되므로 오직 서버측에서만 실행되는 함수
-export const getServerSideProps = () => {
-  const data = 'hello';
+export const getServerSideProps = async () => {
+  // ver1. 직렬구조 (한 호출이 끝나야 다음 호출을 진행)
+  // const allBooks = await fetchBooks();
+  // const recoBooks = await fetchRandomBooks();
+
+  // ver2. 병렬 구조로 변경
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
 
   return {
-    props: { data },
+    props: { allBooks, recoBooks },
   };
 };
 export default function Home({
-  data,
+  allBooks,
+  recoBooks,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   // next에서 제공하는 타입으로 타입을 자동 추천해줌
-  console.log(data);
+
+  console.log(allBooks);
 
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
