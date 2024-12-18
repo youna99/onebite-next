30d2801098655d491780ed9fcf3ba069de687263
youna <dbsk0108@gmail.com>
2024-12-18 21:05:18 +0900

    SSG 동적 경로 적용

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -1,13 +1,32 @@
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
 import style from './[id].module.css';
 import fetchOneBook from '@/lib/fetch-one-books';
+import { useRouter } from 'next/router';
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      { params: { id: '1' } },
+      { params: { id: '2' } },
+      { params: { id: '3' } },
+    ],
+    // 대비책
+    // fallback: false, // 1,2,3이 아니면 다 없는 페이지로 인식
+    // fallback: 'blocking', // (Like SSR )
+    fallback: true, // SSR 방식 + 데이터가 없는 폴백 상태의 페이지부터 반환
+  };
+};
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   console.log('id>>>', id);
   const book = await fetchOneBook(Number(id));
+
+  if (!book) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: { book },
@@ -16,8 +35,11 @@ export const getServerSideProps = async (
 
 export default function Page({
   book,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+  if (router.isFallback) return '로딩중입니다.';
   if (!book) return '문제가 발생했습니다 다시 시도하세요';
+
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
 
   return (
