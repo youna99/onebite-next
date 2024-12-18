0b8ca6963484c5be27ef4acd355b948848b9a24c
youna <dbsk0108@gmail.com>
2024-12-18 20:32:01 +0900

    SSG 정적 경로 적용

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -3,16 +3,22 @@ import { ReactNode } from 'react';
 import style from './index.module.css';
 import SearchableLayout from '@/components/searchable-layout';
 import BookItem from '@/components/book-item';
-import { InferGetServerSidePropsType } from 'next';
+import { InferGetStaticPropsType } from 'next';
 import fetchBooks from '@/lib/fetch-books';
 import fetchRandomBooks from '@/lib/fetch-random-books';
 
+// #1. SSR (getServerSideProps 함수)
 // 페이지 컴포넌트보다 먼저 실행되어, 컴포넌트에 필요한 데이터 불러오는 함수
 // 사전 렌더링하는 과정에서 딱 한번만 실행되므로 오직 서버측에서만 실행되는 함수
-export const getServerSideProps = async () => {
+
+// #2. SSG - 정적 경로 (getStaticPros 함수)
+
+export const getStaticProps = async () => {
   // ver1. 직렬구조 (한 호출이 끝나야 다음 호출을 진행)
   // const allBooks = await fetchBooks();
   // const recoBooks = await fetchRandomBooks();
+
+  console.log('인덱스 페이지');
 
   // ver2. 병렬 구조로 변경
   const [allBooks, recoBooks] = await Promise.all([
@@ -27,10 +33,10 @@ export const getServerSideProps = async () => {
 export default function Home({
   allBooks,
   recoBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  // next에서 제공하는 타입으로 타입을 자동 추천해줌
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  // next에서 제공하는 타입으로 타입을 자동 추천해줌 (#1, #2 동일한 역할할)
 
-  console.log(allBooks);
+  // console.log(allBooks);
 
   return (
     <div className={style.container}>
--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,24 +1,58 @@
 import SearchableLayout from '@/components/searchable-layout';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import BookItem from '@/components/book-item';
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import fetchBooks from '@/lib/fetch-books';
+import { useRouter } from 'next/router';
+import { BookData } from '@/types';
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  console.log(context);
-  const q = context.query.q;
-  const books = await fetchBooks(q as string);
+// #1. SSR
+// export const getServerSideProps = async (
+//   context: GetServerSidePropsContext
+// ) => {
+//   console.log(context);
+//   const q = context.query.q;
+//   const books = await fetchBooks(q as string);
 
-  return {
-    props: { books },
+//   return {
+//     props: { books },
+//   };
+// };
+
+// #2. SSR(정적 경로)
+// SSR에서는 query을 꺼내서 사용할 수 없다
+// 빌드 시점에 쿼리스트링을 알 수 없기 때문
+// 따라서 getStaticProps 방식(서버)이 아닌 클라이언트측에서 직접 진행해야함
+
+// #1. SSR
+// export default function Page({
+//   books,
+// }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+//   return (
+//     <div>
+//       {books.map((book) => (
+//         <BookItem key={book.id} {...book} />
+//       ))}
+//     </div>
+//   );
+// }
+
+// #2. SSG
+export default function Page() {
+  const [books, setBooks] = useState<BookData[]>([]);
+  const router = useRouter();
+  const q = router.query.q;
+
+  const fetchSearchResult = async () => {
+    const data = await fetchBooks(q as string);
+    setBooks(data);
   };
-};
 
-export default function Page({
-  books,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  useEffect(() => {
+    if (q) {
+      fetchSearchResult();
+    }
+  }, [q]);
+
   return (
     <div>
       {books.map((book) => (
